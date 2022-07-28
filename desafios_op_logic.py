cd32433f0ea5953251a832487135df58473d7219
Heoxy <joaofelipe.ferreira15@hotmail.com>
2022-07-28 01:25:46 -0300

    add os arquivos de desafos iniciais do python

---
+++ desafios_op_logic.py
@@ -0,0 +1,11 @@
+trab_terca = True
+trab_quint = False
+
+if trab_terca == True and trab_quint == True:
+    print("Comprar Tv de 50 e tomar sorvete")
+
+if trab_terca == True or trab_quint == True:
+    print("Comprar TV de 32 e tomar sorvete")
+
+if trab_terca == False and trab_quint == False:
+    print("Ficar em casa")
