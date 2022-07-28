fee1fe10e3b4db07760ed806ed69dc33dd8e72f6
Heoxy <joaofelipe.ferreira15@hotmail.com>
2022-07-28 01:35:44 -0300

    arrumando flake8

--- desafios_op_logic.py
+++ desafios_op_logic.py
@@ -1,11 +1,11 @@
 trab_terca = True
 trab_quint = False
 
-if trab_terca == True and trab_quint == True:
+if trab_terca is True and trab_quint is True:
     print("Comprar Tv de 50 e tomar sorvete")
 
-if trab_terca == True or trab_quint == True:
+if trab_terca is True or trab_quint is True:
     print("Comprar TV de 32 e tomar sorvete")
 
-if trab_terca == False and trab_quint == False:
+if trab_terca is False and trab_quint is False:
     print("Ficar em casa")
